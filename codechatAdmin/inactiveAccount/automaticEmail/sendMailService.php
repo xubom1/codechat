22925c6ac17c535211c93af892f61bb706f93e4e
Rakulan7 <s.rakulan04@gmail.com>
2023-05-05 15:46:07 +0200

    check

--- codechatAdmin/inactiveAccount/automaticEmail/sendMailService.php
+++ codechatAdmin/inactiveAccount/automaticEmail/sendMailService.php
@@ -2,55 +2,10 @@
 
 <?php
 
-function Database(){
-    try{
-        $db = new PDO(
-            'mysql:host=localhost;dbname=codechat;charset=utf8',
-            'root',
-            'esgi',
-            [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-        );
-    }
-    catch (Exception $e){
-        die("failed to connect to the database !\n errors : " . $e->getMessage());
-    }
-    return $db;
-}
+include('../../../database.php');
+$db = getDatabase();
 
-$db = Database();
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-require '../../lib/PHPMailer/src/Exception.php';
-require '../../lib/PHPMailer/src/PHPMailer.php';
-require '../../lib/PHPMailer/src/SMTP.php';
-
-function sendMailA($setFromMail, $setFromPseudo, $userMail, $user, $attachementName, $attachementPath, $subject, $body, $bodyNoHtml){
-    $mail = new PHPMailer(true);
-    $mail->isSMTP();
-    $mail->Host       = 'pro1.mail.ovh.net';
-    $mail->SMTPAuth   = true;
-    $mail->Username   = 'support@codechat.fr';
-    $mail->Password   = 'Respons11!';
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->SMTPAutoTLS = true;
-    $mail->Port       = 587;
-
-    $mail->setFrom($setFromMail, $setFromPseudo);
-    $mail->addAddress($userMail, $user);
-
-    if (isset($attachementPath) AND isset($attachementName)){
-        $mail->addAttachment($attachementPath, $attachementName);
-    }
-
-    $mail->isHTML(true);
-    $mail->Subject = $subject;
-    $mail->Body    = $body;
-    $mail->AltBody = $bodyNoHtml;
-
-    $mail->send();
-}
+include('../../mailFunction.php');
 
 date_default_timezone_set('Europe/Paris');
 
@@ -79,7 +34,7 @@ while(true){
                     $subject = 'Your account gonna be deleted';
                     foreach ($showAll as $user) {
                         $body = 'Hello ' . $user['pseudo'] . ',<br> Your account will be deleted if you do not log in. Here is the access link: codechat.fr';
-                        sendMailA('support@codechat.fr', 'Codechat Support', $user['mail'], $user['pseudo'], NULL, NULL, $subject, $body, $body);
+                        sendMail('support@codechat.fr', 'Codechat Support', $user['mail'], $user['pseudo'], NULL, NULL, $subject, $body, $body);
                     }
                     $update = $db->prepare('UPDATE send SET lastSendDate = ?, lastSendTime = ? WHERE id = ?');
                     $update->execute([date('Y-m-d'), date('H:i'), 1]);
