887c45cefac5529f794778fff939d628cbf70af7
Rakulan7 <s.rakulan04@gmail.com>
2023-05-06 16:59:12 +0200

    test Newsletter

--- codechatAdmin/newsletter/index.php
+++ codechatAdmin/newsletter/index.php
@@ -6,8 +6,23 @@ checkSessionAdminElseLogin('../');
 include("../../database.php");
 $db = getDatabase();
 
+function error()
+{
+    if (isset($_GET['msg']) && !empty($_GET['msg'])) {
+        return htmlspecialchars($_GET['msg']);
+    }
+}
+
 $content = '
-    <h1 class="text-center">Newsletter Management</h1>
+    <h1 class="text-center">Newsletter Management</h1>';
+
+if (isset($_GET['msg']) && !empty($_GET['msg'])) {
+    $content .= "<p class='text-center mt-4" . ((isset($_GET['err']) && $_GET['err'] == 'true') ? " alert alert-danger" : " alert alert-success") . "'> 
+    " . error() . "
+    </p>";
+    }
+
+$content .= '
     <div class="container d-flex align-items-start mt-4">
       <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
         <button class="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-create">Create</button>
@@ -19,7 +34,7 @@ $content = '
         <!-- Create Newsletter -->
         <div class="tab-pane fade show active" id="v-pills-create">
         
-            <form class="row g-3 needs-validation" action="check.php" method="POST">
+            <form class="row g-3 needs-validation" action="sendNow.php" method="POST">
                 
                 <select class="form-select" aria-label="Default select example" name="sendTo">
                   <option selected>Send to</option>
@@ -44,7 +59,33 @@ $content = '
         
         <!-- programmed newsletter -->
         <div class="tab-pane fade" id="v-pills-program">
-            <button class="btn btn-primary">Create</button>
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#prgmNews">Program newsletter</button>
+            <div class="modal fade" id="prgmNews" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Create newsletter</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
+                    <form action="service/newsletterService.php" method="post">
+                        <label>Title</label>
+                        <input class="form-control mt-1">
+                        <label class="mt-2">Content</label>
+                        <textarea class="form-control mt-1" style="max-height: 250px; min-height: 250px"></textarea>
+                        <label class="mt-2">Date</label>
+                        <input class="form-control mt-1" type="date">
+                        <label class="mt-2">Time</label>
+                        <input class="form-control mt-1" type="time">
+                    </form>
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Programme</button>
+                  </div>
+                </div>
+              </div>
+            </div>
             <table class="table table-striped">
               <thead>
                 <tr>
---
+++ codechatAdmin/newsletter/service/newsletterService.php
@@ -0,0 +1,32 @@
+#! /usr/bin/env php
+
+<?php
+
+include('/var/www/html/codechat/database.php');
+$db = getDatabase();
+
+include('/var/www/html/codechat/codechatAdmin/mailFunction.php');
+
+$sendMail = $db->prepare('SELECT * FROM newsletter WHERE sent = 0 AND sendDateTime is NULL');
+
+$sendMail->execute([]);
+$getMail = $sendMail->fetchAll(PDO::FETCH_ASSOC);
+
+date_default_timezone_set('Europe/Paris');
+
+foreach ($getMail as $mail){
+    if ($mail['sendTo'] == 'all'){
+        $cmd = $db->prepare('SELECT pseudo, mail FROM user;');
+        $cmd->execute([]);
+        $getUser = $cmd->fetchAll(PDO::FETCH_ASSOC);
+    } else {
+        $cmd = $db->prepare('SELECT pseudo, mail FROM user WHERE admin = ?;');
+        $cmd->execute([1]);
+        $getUser = $cmd->fetchAll(PDO::FETCH_ASSOC);
+    }
+    foreach ($getUser as $user) {
+        sendMail('newsletter@codechat.fr', 'Newsletter', $user['mail'], $user['pseudo'], NULL, NULL, $mail['title'], $mail['content'], $mail['content'], NULL);
+    }
+    $cmd = $db->prepare('UPDATE newsletter SET sendDateTime = ? WHERE id = ?');
+    $cmd->execute([date('Y-m-d H:i:s'), $mail['id']]);
+}
